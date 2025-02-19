feeff168528d8ea76ca1e4420d8b10e1175eaafe
JoMagni <joel.magni@gmail.com>
2025-02-19 14:24:32 +0100

    IPFS push ok

---
+++ app.mjs
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.static('public')); // Sert les fichiers statiques, comme ton HTML
+
+// Route de test pour vérifier si le serveur fonctionne
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: './public' });
+});
+
+// Démarrer le serveur
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
